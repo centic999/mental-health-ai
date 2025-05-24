2cbdb8ff5b01de955399597b5ddddddb0bf6b7ba
centic999 <125836926+centic999@users.noreply.github.com>
2025-05-24 14:55:35 -0400

    fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,6 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TermsOfService from './components/TermsOfService';
-import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
